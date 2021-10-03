4240fae6f80e71def0c4dc6d8133493ec1b32f2e
Zygimantas <zygimantaskondratavicius@gmail.com>
2021-10-03 19:37:09 +0300

    added bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
+  template: `
+  <div class="container">
+  <app-header></app-header>
+  <!-- <p>hello world</p>
+  <button> press here</button>
+  <p>{{ subtitle }}</p>
+  <p>{{ firstNumber}} </p>
+  <hr/> -->
+
+  <p>{{counter}}</p>
+  <button (click)= "counter = counter +2" >Press here</button>
+  </div>
+`,
+styles: ['button { background-color: salmon }',
+'button:hover { background-color: blue ; color: white}' ]
 })
 export class AppComponent {
   title = 'FirstAngularApp';
